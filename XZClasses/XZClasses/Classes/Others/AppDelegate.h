2fe10e71dc85edfd931b1825f8b14f8411492311
nuoatuo <nuoatuo@126.com>
2017-07-18 11:35:33 +0800

    建立项目
    建立项目

---
+++ XZClasses/XZClasses/Classes/Others/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XZClasses
+//
+//  Created by 夜风 on 2017/7/18.
+//  Copyright © 2017年 夜风. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
